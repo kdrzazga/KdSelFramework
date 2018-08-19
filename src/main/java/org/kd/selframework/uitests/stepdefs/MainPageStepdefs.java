4eb8efa895b7c96bbf47c29e1a831e0863f6c6ca
Krzysztof Drzazga <krzysztof.drzazga@capgemini.com>
2018-08-19 14:03:24 +0200

    Fix

--- src/main/java/org/kd/selframework/uitests/stepdefs/MainPageStepdefs.java
+++ src/main/java/org/kd/selframework/uitests/stepdefs/MainPageStepdefs.java
@@ -5,10 +5,12 @@ import cucumber.api.java.en.When;
 import org.kd.selframework.core.utils.TestLogger;
 import org.kd.selframework.core.utils.TestLoggerSingleton;
 import org.kd.selframework.core.utils.WebDriverSingleton;
+import org.openqa.selenium.WebDriver;
 
 public class MainPageStepdefs {
 
     private final TestLogger logger = TestLoggerSingleton.getInstance();
+    private WebDriver driver = WebDriverSingleton.getInstance();
 
     @When("^I navigate to index site$")
     public void navigate(){
